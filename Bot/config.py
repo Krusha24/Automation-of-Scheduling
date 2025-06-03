d244300945fe1ef9fbfb8dc8ffbceb919e14380c
Krusha24 <davidov2000045@gmail.com>
2025-06-03 10:57:43 +0300

    Удалены config.py из отслеживания и обновлен .gitignore

--- Bot/config.py
+++
@@ -1,3 +0,0 @@
-from aiogram import Bot, Dispatcher
-bot = Bot(token='7113512452:AAHMDGL_M79iUm8n7p5pySrDG5x1tbzNBvw')
-dp = Dispatcher()
--- DataBase/config.py
+++
@@ -1,7 +0,0 @@
-DB_HOST = "127.0.0.1"
-DB_PORT = "5432"
-DB_USER = "postgres"
-DB_PASSWORD = "admin"
-DB_NAME = "MyProject"
-
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
